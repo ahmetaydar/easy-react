9d126daa435fafc6a6914f7f02b49808608407a4
ahmetaydar <a.aydar2014@gmail.com>
2023-04-12 03:08:53 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./styles.css";
+{/* prettier-ignore */}
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const { todos, setTodos } = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          ></input>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {todos.map(function (todo) {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+                <button className="btn btn-danger">Delete</button>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
